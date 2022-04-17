ae9ec61f5e534778f7b4004620aa8000aab50195
Anderson <sr.andersonmendesribeiro@gmail.com>
2022-04-16 22:12:59 -0300

    verify Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,17 @@
-import React, { FC, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
-/* import { HashRouter, Route, Routes } from 'react-router-dom'; */
 
 import './App.css';
 
-import FdPizzaBasicNavMenu from './components/FdPizzaBasicNavMenu/FdPizzaBasicNavMenu';
-import FdPizzaBasicCreateItem from './components/FdPizzaBasicCreateItem/FdPizzaBasicCreateItem';
-import FdPizzaBasicIntro from './components/FdPizzaBasicIntro/FdPizzaBasicIntro';
-import FdPizzaBasicSelect from './components/FdPizzaBasicSelect/FdPizzaBasicSelect';
-import FdPizzaBasicCartShopping from './components/FdPizzaBasicCartShopping/FdPizzaBasicCartShopping';
-import FdPizzaBasicFinalizeTransaction from './components/FdPizzaBasicFinalizeTransaction/FdPizzaBasicFinalizeTransaction';
-
-/* import itemsFile from './files/items.json'; */
-
-import { Data } from '../src/interfaces/Interfaces';
-
 function App() {
 
-  const address = window.location.pathname;
-  
-  //const url = address.substring(address.lastIndexOf("/") + 1, address.length); //window.location.href.split("http://localhost:3000/").pop() ?? '';
-
-  const url = address.split('/');
-
-  const url2 = window.location.origin;
-
-  console.log(url);
-  console.log(url2);
-
-  let data: Data = {
-    url: url[2],
-    firstName: 'fd-app',
-    client: {
-      additionalInfo: '',
-      address: '',
-      code: '',           //unnecessary
-      email: '',
-      id: '',
-      name: '',
-      phoneNumber: '',
-      items: [],
-      type: ''
-    },
-    products: []
-  };
-
-  let data2: Data = {
-    url: 'PizzaABeca',
-    firstName: 'fd-app',
-    client: {
-      additionalInfo: '',
-      address: '',
-      code: '',           //unnecessary
-      email: '',
-      id: '',
-      name: '',
-      phoneNumber: '',
-      items: [],
-      type: ''
-    },
-    products: []
-  };
-
   return (
-    /* <div className="App">
-      <div style={{ width: "100vw" }}> */
-        <Router>
-          <Routes> 
-
-            {/* <Route path={ "/" } element={ <><h1 style={{ color: "white" }}>Anderson</h1><FdPizzaBasicNavMenu data={ data2 } /><FdPizzaBasicCreateItem /></> } />
-            <Route path={ data.firstName + "/" } element={ <><h1 style={{ color: "white" }}>Mendes</h1><FdPizzaBasicNavMenu data={ data2 } /><FdPizzaBasicCreateItem /></> } /> */}
-            
-            <Route path={ data.firstName + "/" } element={ <><h1 style={{ color: "white" }}>Anderson</h1><FdPizzaBasicNavMenu data={ data2 } /><FdPizzaBasicCreateItem /></> } />
-            <Route path={ data.firstName + "/" + url[2] } element={ url[2] != '' ? <><FdPizzaBasicNavMenu data={ data } /><FdPizzaBasicIntro data={ data } /></> : <><FdPizzaBasicCreateItem /></> } />
-            {/* <Route path={ data.firstName + "/624fb30f570d98201f131685" } element={ <><FdPizzaBasicNavMenu data={ data } /><FdPizzaBasicIntro data={ data } /></> } /> */}
-            <Route path={ data.firstName + "/selector"} element={ <><FdPizzaBasicNavMenu data={ data } /><FdPizzaBasicSelect data={ data }/></> } />
-            <Route path={ data.firstName + "/cartshopping"} element={ <><FdPizzaBasicNavMenu data={ data } /><FdPizzaBasicCartShopping /></> } />
-            <Route path={ data.firstName + "/finalizeTransaction"} element={ <><FdPizzaBasicNavMenu data={ data } /><FdPizzaBasicFinalizeTransaction /></> } />
-            <Route path={ data.firstName + "/adm" } element={ <><FdPizzaBasicCreateItem /></> } />
-
-          </Routes>
-        </Router>
-      /* </div>
-    </div> */
+    <Router>
+      <Routes>
+        <Route path={ "/" } element={ <><h1 style={{ color: "white" }}>Anderson Mendes</h1><Link to='/fd-app/about'>APP</Link></> } />
+        <Route path={ "/fd-app/" } element={ <><h1 style={{ color: "white" }}>Anderson</h1><Link to='/fd-app/about'>APP</Link></> } />
+        <Route path={ "/fd-app/about" } element={ <h1 style={{ color: "white" }}>Mendes</h1> } />
+      </Routes>
+    </Router>
   );
 }
 
